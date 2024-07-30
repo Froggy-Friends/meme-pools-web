e7ca92b3ad01cf4dc9be514f1b295316d74af064
Luka Martin <ldm3334@gmail.com>
2024-07-30 12:28:26 +1000

    Update social links to use logical OR operator.

--- src/components/token-details/TokenSocials.tsx
+++ src/components/token-details/TokenSocials.tsx
@@ -10,13 +10,13 @@ type TokenSocialsParams = {
 export default function TokenSocials({ token }: TokenSocialsParams) {
   return (
     <div className="flex gap-x-4 items-center mt-6">
-      <Link href={token.twitter ? token.twitter : ""}>
+      <Link href={token.twitter || ""}>
         <FaSquareXTwitter size={40} />
       </Link>
-      <Link href={token.telegram ? token.telegram : ""}>
+      <Link href={token.telegram || ""}>
         <FaTelegram size={40} />
       </Link>
-      <Link href={token.website ? token.website : ""}>
+      <Link href={token.website || ""}>
         <FaGlobe size={40} />
       </Link>
     </div>
