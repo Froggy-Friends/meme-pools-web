71173fe8644288776684474b70ed9c3db28896ef
Luka Martin <ldm3334@gmail.com>
2024-08-02 10:33:27 +1000

    Update image import for token comment component.

--- src/app/token/[tokenAddress]/components/TokenComment.tsx
+++ src/app/token/[tokenAddress]/components/TokenComment.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import placeholderImage from "../../../public/pepe-placeholder.png";
+import placeholderImage from "../../../../../public/pepe-placeholder.png"
 
 export default function TokenComment() {
   return (
