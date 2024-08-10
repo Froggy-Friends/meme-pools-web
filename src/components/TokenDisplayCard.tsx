8052290157039ea6feeb494f0bd0c86343e7155d
Jordan Baron <jordannbaron@gmail.com>
2024-08-09 23:17:24 -0400

    readd creator

--- src/components/TokenDisplayCard.tsx
+++ src/components/TokenDisplayCard.tsx
@@ -1,10 +1,10 @@
 "use client";
 import Image from "next/image";
-import { Token } from "@/lib/types";
+import { Token, User } from "@/lib/types";
 import Link from "next/link";
 
 type TokenDisplayCardProps = {
-  token: Token;
+  token: Token & { creator: User };
 };
 
 export default function TokenDisplayCard({ token }: TokenDisplayCardProps) {
@@ -16,18 +16,16 @@ export default function TokenDisplayCard({ token }: TokenDisplayCardProps) {
       <Image src={token.image} alt="token-image" height={100} width={100} />
 
       <div className="flex flex-col">
-        {/* {tokenCreator && (
-          <div className="flex gap-x-2">
-            <Image
-              src={tokenCreator.imageUrl!}
-              alt="creator avatar"
-              height={20}
-              width={20}
-              className="rounded-md"
-            />
-            <p>Created by {tokenCreator.name}</p>
-          </div>
-        )} */}
+        <div className="flex gap-x-2">
+          <Image
+            src={token.creator.imageUrl!}
+            alt="creator avatar"
+            height={20}
+            width={20}
+            className="rounded-md"
+          />
+          <p>Created by {token.creator.name}</p>
+        </div>
 
         <p>market cap...</p>
         <p>replies...</p>
