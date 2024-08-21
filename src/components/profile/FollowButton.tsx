75b0753ca8fa34408a939cb8a79d7cfafd2b8c59
Luka Martin <145173880+LukaMartin@users.noreply.github.com>
2024-08-21 16:56:45 +1000

    Implement tanstack query for follow button optimistic UI updates. (#30)

--- src/components/profile/FollowButton.tsx
+++ src/components/profile/FollowButton.tsx
@@ -3,7 +3,7 @@
 import { handleFollow } from "@/actions/profile/actions";
 import { FollowStatus } from "@/models/follow";
 import { User } from "@prisma/client";
-import { useOptimistic, useState } from "react";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 
 type FollowButtonProps = {
@@ -17,36 +17,63 @@ export default function FollowButton({
   currentUser,
   user,
 }: FollowButtonProps) {
-  const [optomisticIsFollowing, updateIsFollowing] = useOptimistic(
-    isFollowing,
-    (state, newIsFollowing: string) => {
-      return newIsFollowing;
-    }
-  );
-  const [isPending, setIsPending] = useState(false);
+  const queryClient = useQueryClient();
+  const { data } = useQuery({
+    queryKey: ["isFollowing", currentUser.id],
+    initialData: isFollowing,
+  });
+
+  const handleClick = useMutation({
+    mutationKey: ["changeFollow", currentUser.id],
+    mutationFn: async () => {
+      const errorMessage = await handleFollow(data, user, currentUser);
+      if (errorMessage) {
+        throw new Error(errorMessage);
+      }
+    },
+    onMutate: async () => {
+      const previousData = await queryClient.getQueryData([
+        "isFollowing",
+        currentUser.id,
+      ]);
+
+      if (previousData === FollowStatus.UNFOLLOW) {
+        queryClient.setQueryData(
+          ["isFollowing", currentUser.id],
+          FollowStatus.FOLLOW
+        );
+      } else if (previousData === FollowStatus.FOLLOW) {
+        queryClient.setQueryData(
+          ["isFollowing", currentUser.id],
+          FollowStatus.UNFOLLOW
+        );
+      }
+
+      return { previousData };
+    },
+    onError(error, variables, context) {
+      queryClient.setQueryData(
+        ["isFollowing", currentUser.id],
+        context?.previousData
+      );
+      toast.error((error as Error).message);
+    },
+    onSettled: async () => {
+      queryClient.invalidateQueries({
+        queryKey: ["isFollowing", currentUser.id],
+      });
+    },
+  });
 
   return (
     <button
       className="border-black p-1 border rounded-lg font-semibold w-28"
-      disabled={isPending}
-      onClick={async () => {
-        updateIsFollowing(
-          isFollowing === FollowStatus.UNFOLLOW
-            ? FollowStatus.FOLLOW
-            : FollowStatus.UNFOLLOW
-        );
-        setIsPending(true);
-
-        const errorMessage = await handleFollow(isFollowing, user, currentUser);
-        if (errorMessage) {
-          toast.error(errorMessage as string)
-        }
-        setIsPending(false);
-      }}
+      disabled={handleClick.isPending}
+      onClick={() => handleClick.mutate()}
     >
-      {!isFollowing && FollowStatus.FOLLOW}
-      {optomisticIsFollowing === FollowStatus.UNFOLLOW && FollowStatus.FOLLOW}
-      {optomisticIsFollowing === FollowStatus.FOLLOW && FollowStatus.UNFOLLOW}
+      {!data && FollowStatus.FOLLOW}
+      {data === FollowStatus.UNFOLLOW && FollowStatus.FOLLOW}
+      {data === FollowStatus.FOLLOW && FollowStatus.UNFOLLOW}
     </button>
   );
 }
