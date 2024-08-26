4f88dc1914ca8d21131a24b24a6937aaabf2545c
Luka Martin <ldm3334@gmail.com>
2024-08-26 15:01:47 +1000

    Add user null check to getUserDisplayName function

--- src/lib/getUserDisplayName.ts
+++ src/lib/getUserDisplayName.ts
@@ -2,7 +2,9 @@ import { usernameDisplayLength } from "@/config/user";
 import { User } from "@prisma/client";
 
 export const getUserDisplayName = (user: User) => {
-  if (user.name.length > usernameDisplayLength) {
+  if (!user) {
+    return "----";
+  } else if (user.name.length > usernameDisplayLength) {
     return user.name.substring(0, usernameDisplayLength) + "...";
   } else {
     return user.name;
