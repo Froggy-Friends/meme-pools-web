ac805bfe2d03325f27da08547695c88d6640d8bc
Jordan Baron <jordannbaron@gmail.com>
2024-09-04 01:25:15 -0400

    update launch token button

--- src/components/LaunchCoinButton.tsx
+++ src/components/LaunchCoinButton.tsx
@@ -10,15 +10,12 @@ export default function LaunchCoinButton() {
   const { chain } = useChain();
 
   return (
-    <section className="relative w-56 h-72 flex flex-col items-center justify-center gap-y-10 bg-dark-gray rounded-xl">
-      <Image src={rocket} alt="rocket" className="mt-10" />
-
-      <button
-        className="text-2xl text-dark px-6 py-3 font-proximaSoftBold bg-green rounded-full hover:bg-light-green active:scale-[0.97] transition"
-        onClick={() => router.push(`${chain}/launch`)}
-      >
-        LAUNCH
-      </button>
-    </section>
+    <button
+      onClick={() => router.push(`${chain}/launch`)}
+      className="flex items-center gap-2.5 rounded-lg bg-green px-4 py-2 font-proximaSoftBold text-black hover:bg-opacity-80 transition-all"
+    >
+      <Image src={rocket} alt="rocket" />
+      Launch Token
+    </button>
   );
 }
