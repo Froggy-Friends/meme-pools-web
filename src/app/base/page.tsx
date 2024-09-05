6b3d34a021f13d8108342faa243c159e7b49286a
Jordan Baron <jordannbaron@gmail.com>
2024-09-05 02:27:39 -0400

    leaderboard

--- src/app/base/page.tsx
+++ src/app/base/page.tsx
@@ -1,21 +1,39 @@
-import LaunchCoinButton from "@/components/LaunchCoinButton";
-import TokenDisplayContainer from "@/components/TokenDisplayContainer";
-import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import { Chain } from "@/models/chain";
-import VotingLeaderboard from "@/components/VotingLeaderboard";
-import NewsContainer from "@/components/NewsContainer";
+import Header from "@/components/Header";
+import TokenCarousel from "@/components/TokenCarousel";
+import TokenDisplayCard from "@/components/TokenDisplayCard";
+import TokenDisplayContainer from "@/components/TokenDisplayContainer";
 import TokenSearch from "@/components/TokenSearch";
+import VotingLeaderboard from "@/components/VotingLeaderboard";
+import { Chain } from "@/models/chain";
+import { fetchTopVotesTokens } from "@/queries/token/queries";
 
-export default function BaseHomePage() {
+export default async function BaseHomePage() {
+  const tokens = await fetchTopVotesTokens();
   return (
     <main className="flex flex-col min-h-[100vh] mx-32 px-4">
       <Header chain={Chain.Base} />
 
       <div className="flex flex-col gap-6">
-        <div className="flex justify-between">
+        {/* <div className="flex justify-between">
           <NewsContainer />
           <VotingLeaderboard />
+        </div> */}
+
+        <div className="h-[400px] rounded-lg bg-dark-gray p-4 flex flex-col gap-6">
+          <div className="flex items-center justify-between w-full">
+            <span className="font-proximaSoftBold uppercase">Leaderboard</span>
+            <VotingLeaderboard tokens={tokens} />
+          </div>
+          <div className="bg-dark px-2.5 py-10 flex items-center gap-2.5 overflow-x-auto overflow-y-hidden rounded-lg w-full h-[320px]">
+            {tokens.map((token) => (
+              <TokenDisplayCard
+                key={token.id}
+                token={token}
+                layout="horizontal"
+              />
+            ))}
+          </div>
         </div>
 
         <TokenSearch />
--- src/components/VotingLeaderboard.tsx
+++ src/components/VotingLeaderboard.tsx
@@ -1,59 +1,32 @@
+import { Token } from "@prisma/client";
 import Image from "next/image";
+import bronzeMedal from "../../public/bronze-medal.svg";
 import goldMedal from "../../public/gold-medal.svg";
 import silverMedal from "../../public/silver-medal.svg";
-import bronzeMedal from "../../public/bronze-medal.svg";
-import { PiArrowFatUpFill } from "react-icons/pi";
-import { fetchTopVotesTokens } from "@/queries/token/queries";
-import SeeAllButton from "./SeeAllButton";
 
-export default async function VotingLeaderboard() {
-  const tokens = await fetchTopVotesTokens();
-
+export default function VotingLeaderboard({ tokens }: { tokens: Token[] }) {
   return (
-    <section className="w-[400px] h-72 flex flex-col p-5 rounded-lg bg-dark-gray">
-      <h3 className="text-2xl font-proximaSoftBold pb-6">Voting Leaderboard</h3>
+    <section className="flex items-center gap-6">
+      {tokens.map((token, index) => (
+        <div className="flex items-center gap-1" key={token.id}>
+          <Image
+            src={
+              index === 0 ? goldMedal : index === 1 ? silverMedal : bronzeMedal
+            }
+            alt="gold-medal"
+            height={14}
+            width={14}
+          />
 
-      {tokens.map((token, index) => {
-        return (
-          <div
-            className="flex items-center justify-between mb-6"
-            key={token.id}
-          >
-            <div className="flex items-center">
-              <Image
-                src={
-                  index === 0
-                    ? goldMedal
-                    : index === 1
-                    ? silverMedal
-                    : bronzeMedal
-                }
-                alt="gold-medal"
-                height={35}
-                width={35}
-                className="mr-4"
-              />
+          <span className="text-sm">
+            {index === 0 ? "1st" : index === 1 ? "2nd" : "3rd"}
+          </span>
 
-              <Image
-                src={token.image}
-                alt="profile-image"
-                height={35}
-                width={35}
-                className="rounded-lg mr-2"
-              />
-
-              <p className="text-lg">${token.ticker}</p>
-            </div>
-
-            <div className="flex items-center gap-x-1">
-              <PiArrowFatUpFill size={25} />
-              <p>{token.voteCount.upVotes}</p>
-            </div>
-          </div>
-        );
-      })}
-
-      <SeeAllButton />
+          <span className="text-xs rounded-[4px] bg-green text-black px-2 py-1 font-proximaSoftBold">
+            $ {token.ticker}
+          </span>
+        </div>
+      ))}
     </section>
   );
 }
