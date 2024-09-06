cff149d30efc35ea8450e56bcf1df26fd3a05d4b
Jordan Baron <jordannbaron@gmail.com>
2024-09-06 02:18:41 -0400

    add useTokens hook

---
+++ src/hooks/useTokens.ts
@@ -0,0 +1,15 @@
+import { TokenFilter } from "@/models/token";
+import { fetchTokens } from "@/queries/token/queries";
+import { useQuery } from "@tanstack/react-query";
+
+export default function useTokens(filter: TokenFilter, page: number) {
+  const {data: tokens, isPending: isLoadingTokens} = useQuery({
+    queryKey: ["tokens", filter, page],
+    queryFn: () => fetchTokens(filter, page),
+  })
+
+  return {
+    tokens,
+    isLoadingTokens
+  }
+}
