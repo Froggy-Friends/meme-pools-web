acbff6a9f8641ef4b699e5a8f59445ee4eccf8f2
Fonzy <fonzy@fonzy.io>
2024-09-14 08:48:44 -0700

    Remove token table tokenId column (#53)

---
+++ prisma/migrations/20240914082441_remove_token_table_token_id/migration.sql
@@ -0,0 +1,11 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `tokenId` on the `Token` table. All the data in the column will be lost.
+
+*/
+-- DropIndex
+DROP INDEX "Token_tokenId_key";
+
+-- AlterTable
+ALTER TABLE "Token" DROP COLUMN "tokenId";
