ed14da5e6d5c84aa7771787bd6af1f030d140a57
0xFonzy <fonzy@fonzy.io>
2024-09-15 12:34:37 -0700

    style connect button

--- src/components/eth/EvmConnectButton.tsx
+++ src/components/eth/EvmConnectButton.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { useWeb3Modal } from "@web3modal/wagmi/react";
 
@@ -8,7 +8,7 @@ export default function EvmConnectButton() {
   return (
     <>
       <button
-        className="py-2 px-4 rounded-md bg-green text-dark text-xl font-proximaSoftBold hover:bg-light-green active:scale-[0.97] transition"
+        className="flex items-center h-8 w-38 py-2 px-4 rounded-md bg-green text-dark text-sm font-proximaSoftBold hover:bg-light-green active:scale-[0.97] transition"
         onClick={() => open()}
       >
         Connect
