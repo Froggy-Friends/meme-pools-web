8bf448446fe8c904b6a5713bc52c4824638d254d
alfonsoaranzazu <fonzy@fonzy.io>
2024-09-14 22:36:41 -0700

    fix launch route

--- src/components/LaunchCoinButton.tsx
+++ src/components/LaunchCoinButton.tsx
@@ -11,7 +11,7 @@ export default function LaunchCoinButton() {
 
   return (
     <button
-      onClick={() => router.push(`${chain}/launch`)}
+      onClick={() => router.push(`/${chain}/launch`)}
       className="flex items-center gap-2.5 rounded-lg bg-green px-4 py-2 font-proximaSoftBold text-black hover:bg-opacity-80 transition-all"
     >
       <Image src={rocket} alt="rocket" />
