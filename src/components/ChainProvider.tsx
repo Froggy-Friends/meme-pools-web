32595179a840e01ca6869e9d5b6f81f0bcb8e54a
0xFonzy <fonzy@fonzy.io>
2024-09-16 20:54:05 -0700

    prompt chain switch

--- src/components/ChainProvider.tsx
+++ src/components/ChainProvider.tsx
@@ -1,15 +1,23 @@
 "use client";
 
-import { useState, FC, ReactNode } from "react";
+import { useState, FC, ReactNode, useEffect } from "react";
 import { ChainContext } from "@/context/chain";
-import { ChainConfig } from "@/models/chain";
+import { Chain, ChainConfig } from "@/models/chain";
 import { usePathname } from "next/navigation";
 import { getChainConfig } from "@/lib/chains";
+import { useSwitchChain } from "wagmi";
 
 export const ChainProvider: FC<{ children: ReactNode }> = ({ children }) => {
+  const { switchChain } = useSwitchChain();
   const pathname = usePathname();
   const chainConfig = getChainConfig(pathname);
   const [chain, setChain] = useState<ChainConfig>(chainConfig);
+
+  useEffect(() => {
+    if (chain.name === Chain.Eth || chain.name === Chain.Base) {
+      switchChain({ chainId: chain.id });
+    }
+  }, [chain, chain.name, switchChain]);
 
   return <ChainContext.Provider value={{ chain, setChain }}>{children}</ChainContext.Provider>;
 };
