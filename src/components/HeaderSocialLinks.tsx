b9756a2cd62101a8518bf51d5c51c9a02ad7ec15
0xFonzy <fonzy@fonzy.io>
2024-09-17 00:01:17 -0700

    style popover

--- src/components/HeaderSocialLinks.tsx
+++ src/components/HeaderSocialLinks.tsx
@@ -5,16 +5,12 @@ import { FaSquareXTwitter, FaTelegram } from "react-icons/fa6";
 
 export default function HeaderSocialLinks() {
   return (
-    <div className="flex items-center gap-x-4 mt-14">
-      <FaTelegram
-        size={35}
-        className="hover:text-white/80 hover:scale-[1.03] transition"
-      />
+    <div className="flex items-center gap-x-4 mt-20">
+      <Link href="https://t.me/Frog_HQ" target="_blank">
+        <FaTelegram size={35} className="hover:text-green hover:scale-[1.03] transition" />
+      </Link>
       <Link href="https://x.com/frogdotfun" target="_blank">
-        <FaSquareXTwitter
-          size={37}
-          className="hover:text-white/80 hover:scale-[1.03] transition"
-        />
+        <FaSquareXTwitter size={37} className="hover:text-green hover:scale-[1.03] transition" />
       </Link>
     </div>
   );
--- src/components/LogoPopover.tsx
+++ src/components/LogoPopover.tsx
@@ -1,19 +1,27 @@
+"use client";
+
 import { Popover, PopoverTrigger, PopoverContent } from "@nextui-org/react";
 import HeaderSocialLinks from "./HeaderSocialLinks";
 import frogFunLogo from "../../public/frog-fun-logo.svg";
 import Image from "next/image";
 import { IoIosArrowDown } from "react-icons/io";
+import { useState } from "react";
+import Link from "next/link";
 
 export default function LogoPopover() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <Popover
+      isOpen={isOpen}
+      onOpenChange={setIsOpen}
       placement="bottom-start"
       classNames={{
-        content: ["bg-dark-gray", "rounded-lg", "p-5"],
+        content: ["bg-dark-gray", "rounded-lg", "p-5", "mt-2", "w-[250px]", "items-start"],
       }}
     >
       <PopoverTrigger className="cursor-pointer">
-        <div className="flex items-center gap-x-1">
+        <div className="flex items-center gap-x-1" onMouseEnter={() => setIsOpen(true)}>
           <Image
             src={frogFunLogo}
             alt="Frog Fun Logo"
@@ -24,14 +32,18 @@ export default function LogoPopover() {
           <IoIosArrowDown size={25} />
         </div>
       </PopoverTrigger>
-      <PopoverContent>
+      <PopoverContent onMouseLeave={() => setIsOpen(false)}>
         <div className="flex flex-col">
-          <h2 className="text-5xl font-bold">
+          <h2 className="text-2xl font-bold">
             <span className="text-green">FROG</span>.FUN
           </h2>
 
-          <p className="text-lg pt-4">Privacy Policy</p>
-          <p className="text-lg pt-4">Terms of Service</p>
+          <Link href="https://docs.frog.fun/privacy" target="_blank">
+            <p className="cursor-pointer text-lg pt-6 hover:text-green">Privacy Policy</p>
+          </Link>
+          <Link href="https://docs.frog.fun/terms" target="_blank">
+            <p className="cursor-pointer text-lg pt-4 hover:text-green">Terms of Service</p>
+          </Link>
 
           <HeaderSocialLinks />
         </div>
