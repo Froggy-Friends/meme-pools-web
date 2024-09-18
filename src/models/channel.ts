c518104fc611045398ae5a79cdc602c6e7d9a083
Jordan Baron <jordannbaron@gmail.com>
2024-09-18 16:09:28 -0400

    rm comments

--- src/models/channel.ts
+++ src/models/channel.ts
@@ -1,12 +1,12 @@
 export enum Channel {
-  Comment = "comment", // ✓
+  Comment = "comment",
   CommentLikes = "comment-likes",
   CommentDislikes = "comment-dislikes",
-  Upvotes = "votes", // ✓
-  Downvotes = "downvotes", // ✓
-  Follow = "follow", // ✓
-  Unfollow = "unfollow", // ✓
-  CreateToken = "create-token", // ? - create token is failing
+  Upvotes = "votes",
+  Downvotes = "downvotes",
+  Follow = "follow",
+  Unfollow = "unfollow",
+  CreateToken = "create-token",
   Buy = "buy", // TODO - implement when buy/sell is implemented
   Sell = "sell", // TODO - implement when buy/sell is implemented
 }
