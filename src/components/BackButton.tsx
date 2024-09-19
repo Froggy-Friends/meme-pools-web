a255d73d9d254ceac2c18f67c06fe8d9fc43e067
alfonsoaranzazu <fonzy@fonzy.io>
2024-09-18 22:37:23 -0700

    style back button

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -7,7 +7,7 @@ export default function BackButton() {
 
   return (
     <button
-      className="bg-dark-gray border-[0.25px] border-white/[5%] w-[65px] h-[35px] rounded-3xl px-3 py-2 mt-10 mb-6 text-small hover:bg-gray active:scale-[0.97] transition"
+      className="flex items-center justify-center bg-dark-gray border-[0.25px] border-white/[5%] w-[65px] h-[35px] rounded-3xl px-3 py-2 mt-10 mb-6 text-small hover:bg-gray active:scale-[0.97] transition"
       onClick={() => router.back()}
     >
       Back
