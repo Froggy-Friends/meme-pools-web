b912d2ee7790a2638dfd20f2ed9123b4865a791c
alfonsoaranzazu <fonzy@fonzy.io>
2024-09-19 01:17:03 -0700

    style back button

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -1,16 +1,17 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import { IoMdArrowDropleft } from "react-icons/io";
 
 export default function BackButton() {
   const router = useRouter();
 
   return (
     <button
-      className="flex items-center justify-center bg-dark-gray border-[0.25px] border-white/[5%] w-[65px] h-[35px] rounded-3xl px-3 py-2 mt-10 mb-6 text-small hover:bg-gray active:scale-[0.97] transition"
+      className="flex items-center justify-center bg-dark-gray border-[0.25px] border-white/[5%] w-10 h-8 rounded-3xl text-small hover:bg-gray active:scale-[0.97] transition"
       onClick={() => router.back()}
     >
-      Back
+      <IoMdArrowDropleft size={24} />
     </button>
   );
 }
