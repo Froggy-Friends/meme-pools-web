19a3435a623aea8df1fecca3463b90bbdb93578a
alfonsoaranzazu <fonzy@fonzy.io>
2024-09-19 00:40:19 -0700

    add show search prop to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import ProfileAvatar from "./ProfileAvatar";
-import { cookies } from "next/headers";
+import { cookies, headers } from "next/headers";
 import { fetchUser } from "@/queries/profile/queries";
 import { Chain } from "@/models/chain";
 import ChainSwitcher from "./ChainSwitcher";
@@ -7,22 +7,25 @@ import { Cookie } from "@/models/cookie";
 import LogoPopover from "./LogoPopover";
 import LaunchCoinButton from "./LaunchCoinButton";
 import HowItWorksButton from "./HowItWorksButton";
+import TokenSearch from "./TokenSearch";
+import { usePathname } from "next/navigation";
 
 type HeaderProps = {
   chain: Chain;
+  showSearch?: boolean;
 };
 
-export default async function Header({ chain }: HeaderProps) {
+export default async function Header({ chain, showSearch }: HeaderProps) {
   const cookieStore = cookies();
   const userEvmAddress = cookieStore.get(Cookie.EvmAddress);
   const userSolAddress = cookieStore.get(Cookie.SolanaAddress);
-  const user = await fetchUser(
-    chain === Chain.Eth ? userEvmAddress?.value : userSolAddress?.value
-  );
+  const user = await fetchUser(chain === Chain.Eth ? userEvmAddress?.value : userSolAddress?.value);
 
   return (
-    <header className="flex justify-between items-center h-32">
+    <header className="flex justify-between items-center h-20">
       <LogoPopover />
+
+      {showSearch && <TokenSearch classNames="justify-center" />}
 
       <div className="flex items-center gap-x-4">
         <LaunchCoinButton />
