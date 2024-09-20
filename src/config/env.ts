330c7f3d2e1a510bcc39dd749e094bf68e7565cf
alfonsoaranzazu <fonzy@fonzy.io>
2024-09-20 12:51:29 -0700

    add constants to env config

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,8 +1,16 @@
 export const env = process.env.NEXT_PUBLIC_ENV;
-
 export const isProd = env === "prod";
 
+// api
+export const frogFunApi = isProd ? "https://api.frog.fun" : "https://dev.api.frog.fun";
+
+// evm
+export const contractAddress = isProd ? "0x0" : "0xb5DdD2Ca2E43eE59882075abeefd79a01b5cE914";
 export const ethChainId = isProd ? 1 : 11155111;
 export const baseChainId = isProd ? 8453 : 84532;
-export const contractAddress = isProd ? "0x0" : "0xb5DdD2Ca2E43eE59882075abeefd79a01b5cE914";
-export const frogFunApi = isProd ? "https://api.frog.fun" : "https://dev.api.frog.fun";
+
+// solana
+export const solanaChainId = isProd ? 0 : 0;
+
+// wallet connect
+export const walletConnectProjectId = "28fb63c6591b078e4eca73e608508aec";
