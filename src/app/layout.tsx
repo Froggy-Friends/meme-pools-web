f9ba7b92b7bbb05ffda6df0af9adad2b0e745045
alfonsoaranzazu <fonzy@fonzy.io>
2024-09-20 12:56:36 -0700

    removes moralis

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,6 @@ import { config } from "@/config/wagmi";
 import Web3ModalProvider from "@/context";
 import { NextUIProvider } from "@nextui-org/react";
 import { Toaster } from "react-hot-toast";
-import Moralis from "moralis";
 import AppWalletProvider from "@/components/AppWalletProvider";
 import { ChainProvider } from "@/components/ChainProvider";
 
@@ -21,11 +20,6 @@ export default async function RootLayout({
   children: React.ReactNode;
 }>) {
   const initialState = cookieToInitialState(config, headers().get("cookie"));
-  if (!Moralis.Core.isStarted) {
-    await Moralis.start({
-      apiKey: process.env.MORALIS_API_KEY,
-    });
-  }
 
   return (
     <html lang="en">
