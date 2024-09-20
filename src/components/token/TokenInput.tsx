1dd820e058454eb76736f9515ceb88d3f687d89c
alfonsoaranzazu <fonzy@fonzy.io>
2024-09-20 00:44:40 -0700

    add ticker src prop

--- src/components/token/TokenInput.tsx
+++ src/components/token/TokenInput.tsx
@@ -1,15 +1,14 @@
 import { useState } from "react";
 import Image from "next/image";
 import { Input } from "@nextui-org/react";
-import { ethLogo } from "@/config/chains";
 
 type TokenInputProps = {
   ticker?: string;
-  placeholder?: string;
+  tickerSrc: string;
   onChange?: (value: number | null) => void;
 };
 
-export default function TokenInput({ ticker, placeholder, onChange }: TokenInputProps) {
+export default function TokenInput({ ticker, tickerSrc, onChange }: TokenInputProps) {
   const [value, setValue] = useState<string>("");
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -22,7 +21,7 @@ export default function TokenInput({ ticker, placeholder, onChange }: TokenInput
       return;
     }
 
-    // Regex to match numbers with up to two decimal places
+    // Regex to match numbers with up to ten decimal places
     const regex = /^\d*\.?\d{0,10}$/;
 
     if (regex.test(inputValue)) {
@@ -37,14 +36,14 @@ export default function TokenInput({ ticker, placeholder, onChange }: TokenInput
         input: "ml-8 appearance-none",
         inputWrapper: ["h-[55px] bg-dark data-[hover=true]:bg-dark data-[focus=true]:bg-dark"],
       }}
-      placeholder={placeholder}
+      placeholder="0.0"
       value={value}
       onChange={handleChange}
       type="text"
       radius="full"
       startContent={
         <span className="flex items-center gap-2">
-          <Image src={ethLogo} alt="Eth" width={35} height={35} />
+          <Image src={tickerSrc} alt="ticker" width={35} height={35} />
           <p className="uppercase">${ticker}</p>
         </span>
       }
