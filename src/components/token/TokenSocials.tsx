de7f2afb0a2b53148f358d289a21988acac6b56d
Luka Martin <ldm3334@gmail.com>
2024-10-08 19:20:22 +1100

    Update share link menue

--- src/components/token/TokenSocials.tsx
+++ src/components/token/TokenSocials.tsx
@@ -9,6 +9,7 @@ import Link from "next/link";
 import Image from "next/image";
 import useCopy from "@/hooks/useClipboardCopy";
 import useXPost from "@/hooks/useXPost";
+import { etherscanUrl } from "@/config/env";
 
 type TokenSocialsParams = {
   token: Token;
@@ -30,26 +31,40 @@ export default function TokenSocials({ token }: TokenSocialsParams) {
   return (
     <div className="flex gap-x-1 items-center mt-2 laptop:mt-4">
       <Link href={token.telegram || ""} className={linkStyles}>
-        <FaTelegram className="w-4 h-4 tablet:w-6 tablet:h-6 laptop:w-8 laptop:h-8" />
+        <FaTelegram className="w-4 h-4 tablet:w-6 tablet:h-6" />
       </Link>
       <Link href={token.website || ""} className={linkStyles}>
-        <FaGlobe className="w-4 h-4 tablet:w-6 tablet:h-6 laptop:w-8 laptop:h-8" />
+        <FaGlobe className="w-4 h-4 tablet:w-6 tablet:h-6" />
       </Link>
       <Link href={token.twitter || ""} className={linkStyles}>
-        <FaXTwitter className="w-4 h-4 tablet:w-6 tablet:h-6 laptop:w-8 laptop:h-8" />
+        <FaXTwitter className="w-4 h-4 tablet:w-6 tablet:h-6" />
       </Link>
       <Dropdown>
         <DropdownTrigger className="outline-none">
           <button>
-            <IoMdShareAlt className="w-6 h-6 tablet:w-10 tablet:h-10 laptop:w-12 laptop:h-12" />
+            <IoMdShareAlt className="w-6 h-6 tablet:w-10 tablet:h-10" />
           </button>
         </DropdownTrigger>
         <DropdownMenu aria-label="Share options">
           <DropdownItem key="share-link" onPress={() => handleShareLink()}>
-            Share link
+            Cupy link
           </DropdownItem>
           <DropdownItem key="share-x" onPress={() => handleShareX()}>
-            Share on X
+            Share tweet
+          </DropdownItem>
+          <DropdownItem
+            key="discord"
+            isReadOnly={!token.discord}
+            className={!token.discord ? "cursor-default" : "cursor-pointer"}
+          >
+            <Link href={token.discord || ""} target="_blank">
+              Discord
+            </Link>
+          </DropdownItem>
+          <DropdownItem key="explorer">
+            <Link href={`${etherscanUrl}/token/${token.tokenAddress}`} target="_blank">
+              Explorer
+            </Link>
           </DropdownItem>
         </DropdownMenu>
       </Dropdown>
