c417065f8ec98365782370890b293cdb6b71b0f7
Luka Martin <ldm3334@gmail.com>
2024-10-14 10:48:59 +1100

    Update default profile avatar

--- src/config/user.ts
+++ src/config/user.ts
@@ -1,5 +1,5 @@
 export const defaultProfileAvatarUrl =
-  "https://qojtn2d8pd7yjrch.public.blob.vercel-storage.com/Frog.fun_Default_PFP-nKVI2J7DIDPs8vFvCTnkVxG4aT6asQ.png";
+  "https://qojtn2d8pd7yjrch.public.blob.vercel-storage.com/Default%20Frog-KMUE9RxZ3KIHF6YGnBiR0wm4A9blOB.png";
 export const fetchUserCacheKey = "my-app-user";
 export const fetchUserCacheTag = "user";
 export const usernameDisplayLength = 8;
