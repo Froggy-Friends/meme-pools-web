610d880f4810311f14ce32bb0a7df8281bccdc00
Luka Martin <ldm3334@gmail.com>
2024-10-15 19:32:36 +1100

    Add check for payload isLoading state to stop unwanted rendering

--- src/components/LeaderBoardAndFeedContainer.tsx
+++ src/components/LeaderBoardAndFeedContainer.tsx
@@ -4,6 +4,7 @@ import { Token } from "@prisma/client";
 import LiveFeed from "./LiveFeed";
 import VotingLeaderboard from "./VotingLeaderboard";
 import { useFeatureFlagPayload } from "posthog-js/react";
+import { useEffect, useState } from "react";
 
 type LeaderBoardAndFeedContainerProps = {
   topTokens: Token[];
@@ -11,10 +12,21 @@ type LeaderBoardAndFeedContainerProps = {
 
 export default function LeaderBoardAndFeedContainer({ topTokens }: LeaderBoardAndFeedContainerProps) {
   const payload = useFeatureFlagPayload("spotlight");
-  
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    console.log("payload", payload);
+  }, [payload]);
+
+  useEffect(() => {
+    if (payload !== undefined) {
+      setIsLoading(false);
+    }
+  }, [payload]);
+
   return (
     <div className="mb-[70px] tablet:mb-24  flex flex-col tablet:flex-row items-center gap-2 desktop:gap-4">
-      {!payload && (
+      {!payload && !isLoading && (
         <div className="w-full rounded-lg bg-dark-gray p-4 flex flex-col gap-6">
           <div className="flex items-center justify-between w-full">
             <span className="hidden laptop:block font-proximaSoftBold uppercase">Leaderboard</span>
@@ -23,7 +35,7 @@ export default function LeaderBoardAndFeedContainer({ topTokens }: LeaderBoardAn
         </div>
       )}
 
-      {!payload && <LiveFeed />}
+      {!payload && !isLoading && <LiveFeed />}
     </div>
   );
 }
