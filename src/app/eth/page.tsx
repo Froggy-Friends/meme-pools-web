9a575a3c3922a2684fa99461eacb5e65b319a5b9
Luka Martin <ldm3334@gmail.com>
2024-10-15 19:03:59 +1100

    Add dynamic component import

--- src/app/eth/page.tsx
+++ src/app/eth/page.tsx
@@ -1,14 +1,16 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
-import LeaderBoardAndFeedContainer from "@/components/LeaderBoardAndFeedContainer";
-import LiveFeed from "@/components/LiveFeed";
 import Spotlight from "@/components/Spotlight";
 import TokenPageContent from "@/components/TokenPageContent";
-import VotingLeaderboard from "@/components/VotingLeaderboard";
 import { Chain } from "@/models/chain";
 import { TokenFilter } from "@/models/token";
 import { fetchTokens, fetchTopVotesTokens } from "@/queries/token/queries";
 import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query";
+import dynamic from "next/dynamic";
+
+const DynamicLeaderBoardAndFeedContainer = dynamic(() => import("../../components/LeaderBoardAndFeedContainer"), {
+  ssr: false,
+});
 
 export default async function EthHomePage() {
   const queryClient = new QueryClient();
@@ -26,7 +28,7 @@ export default async function EthHomePage() {
       <Header chain={Chain.Eth} />
       <HydrationBoundary state={dehydrate(queryClient)}>
         <div className="flex flex-col gap-6 w-full">
-          <LeaderBoardAndFeedContainer topTokens={topTokens} />
+          <DynamicLeaderBoardAndFeedContainer topTokens={topTokens} />
 
           <Spotlight />
 
