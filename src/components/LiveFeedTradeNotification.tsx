a87e609bde81437613324597b78fa285071c44c3
Luka Martin <ldm3334@gmail.com>
2024-10-15 14:54:00 +1100

    Add spotlight component. Update Live feed notificaton component

--- src/components/LiveFeedTradeNotification.tsx
+++ src/components/LiveFeedTradeNotification.tsx
@@ -3,24 +3,34 @@ import { getUserDisplayName } from "@/lib/getUserDisplayName";
 import { FormattedTrade } from "@/types/token/types";
 import Image from "next/image";
 
-export function LiveFeedTradeNotification({ trade, isAnimating }: { trade: FormattedTrade; isAnimating: boolean }) {
+type LiveFeedTradeNotificationProps = {
+  trade: FormattedTrade;
+  isAnimating: boolean;
+  spotlight?: boolean;
+};
+
+export function LiveFeedTradeNotification({ trade, isAnimating, spotlight = false }: LiveFeedTradeNotificationProps) {
   return (
     <div
       className={`text-sm font-proximaSoft flex items-center gap-1 transition-all duration-500 ease-in-out ${
         isAnimating ? "-translate-y-full opacity-0" : "translate-y-0 opacity-100"
       }`}
     >
-      <Image
-        src={trade.userAvatar || defaultProfileAvatarUrl}
-        alt="user-profile-picture"
-        height={16}
-        width={16}
-        className="rounded-full mr-1"
-      />
-      <span>{getUserDisplayName(trade.username)}</span>
-      <span className="text-green">bought</span>
-      <span className="text-cream">{trade.amount}</span>
-      <span className="text-light-green">${trade.tokenTicker}</span>
+      {!spotlight && (
+        <Image
+          src={trade.userAvatar || defaultProfileAvatarUrl}
+          alt="user-profile-picture"
+          height={16}
+          width={16}
+          className="rounded-full mr-1"
+        />
+      )}
+      {!spotlight && <span>{getUserDisplayName(trade.username)}</span>}
+      {!spotlight && <span className="text-green">bought</span>}
+      {!spotlight && <span className="text-cream">{trade.amount}</span>}
+      {!spotlight && <span className="text-light-green">${trade.tokenTicker}</span>}
+      {spotlight && <span className="text-green">${trade.usdCost.toFixed(2)}</span>}
+      {spotlight && <span>Buy</span>}
     </div>
   );
 }
--- src/components/Spotlight.tsx
+++ src/components/Spotlight.tsx
@@ -9,10 +9,14 @@ import { useFeatureFlagPayload } from "posthog-js/react";
 import { useState } from "react";
 import Pusher from "pusher-js";
 import { LiveFeedTradeNotification } from "./LiveFeedTradeNotification";
+import { formatTicker } from "@/lib/formatTicker";
+import { useChain } from "@/context/chain";
+import Link from "next/link";
 
 export default function Spotlight() {
   const payload = useFeatureFlagPayload("spotlight");
   const { token } = useToken(payload as string);
+  const { chain } = useChain();
   const [latestTrade, setLatestTrade] = useState<FormattedTrade | null>(null);
   const [isAnimating, setIsAnimating] = useState(false);
 
@@ -55,8 +59,8 @@ export default function Spotlight() {
   return (
     <>
       {payload && token && (
-        <section className="w-full h-[140px] laptop:w-[750px] tablet:h-[200px] flex flex-col p-6 laptop:p-8 bg-dark-gray rounded-xl">
-          <div className="flex items-center justify-between mb-6">
+        <section className="relative overflow-hidden w-full h-[140px] laptop:w-[750px] tablet:h-[200px] flex flex-col p-4 laptop:p-8 bg-dark-gray rounded-xl">
+          <div className="flex items-center justify-between mb-4 tablet:mb-6">
             <h3 className="text-2xl">Spotlight</h3>
             <div className="flex items-center gap-x-1">
               <div className="w-2 h-2 rounded-full bg-green animate-pulse" />
@@ -64,18 +68,48 @@ export default function Spotlight() {
             </div>
           </div>
 
-          <div className="flex items-center justify-between">
-            <div className="flex items-center gap-x-4">
-              <Image src={token.image} alt={token.name} width={60} height={60} className="rounded-full" />
-              <div className="flex flex-col">
-                <p className="text-4xl font-proximaSoftBold mb-2">${token.ticker}</p>
+          <div className="flex items-center w-full">
+            <div className="flex gap-x-2 tablet:gap-x-4">
+              <div className="self-start">
+                <Link href={`${chain.name}/token/${token.tokenAddress}`}>
+                  <Image
+                    src={token.image}
+                    alt={token.name}
+                    width={60}
+                    height={60}
+                    className="rounded-full hover:scale-[1.02] transition"
+                  />
+                </Link>
+              </div>
 
-                <div className="flex items-center justify-between">
-                  <p className="text-sm text-white/90">{token.description}</p>
-                  {latestTrade && <LiveFeedTradeNotification trade={latestTrade} isAnimating={isAnimating} />}
-                </div>
+              <div className="flex flex-col w-full">
+                <Link
+                  href={`${chain.name}/token/${token.tokenAddress}`}
+                  className="hidden tablet:block tablet:text-4xl font-proximaSoftBold text-white/80 hover:text-white transition tablet:mb-2"
+                >
+                  ${token.ticker}
+                </Link>
+                <Link
+                  href={`${chain.name}/token/${token.tokenAddress}`}
+                  className="block tablet:hidden text-2xl text-white/80 hover:text-white transition"
+                >
+                  ${formatTicker(token.ticker)}
+                </Link>
+
+                <p className="hidden tablet:block text-sm text-white/90 max-w-[55%] max-h-[45px] overflow-y-hidden">
+                  {token.description}
+                </p>
+                <p className="block tablet:hidden -mt-1 text-sm text-white/90 max-w-[90%] max-h-[45px] overflow-hidden whitespace-nowrap text-ellipsis">
+                  {token.description}
+                </p>
               </div>
             </div>
+          </div>
+
+          <div className="absolute right-4 tablet:right-8 bottom-[28px] tablet:bottom-[50px]">
+            {latestTrade && (
+              <LiveFeedTradeNotification trade={latestTrade} isAnimating={isAnimating} spotlight={true} />
+            )}
           </div>
         </section>
       )}
