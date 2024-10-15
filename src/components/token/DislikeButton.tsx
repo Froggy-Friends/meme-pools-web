2e49ac4b9eed08581a3803a412b8623ee47eb560
Luka Martin <ldm3334@gmail.com>
2024-10-15 21:39:24 +1100

    Add minor UX enhancements

--- src/components/token/DislikeButton.tsx
+++ src/components/token/DislikeButton.tsx
@@ -4,6 +4,7 @@ import { FaThumbsDown, FaRegThumbsDown } from "react-icons/fa6";
 import { HandleDislike } from "../../types/token/types";
 import { CommentLikes } from "@prisma/client";
 import { DefinedUseQueryResult } from "@tanstack/react-query";
+import useUser from "@/hooks/useUser";
 
 type DislikeButtonProps = {
   dislikes: DefinedUseQueryResult<number, Error>;
@@ -16,11 +17,13 @@ export default function DislikeButton({
   commentDisLike,
   handleDislike,
 }: DislikeButtonProps) {
+  const { currentUser } = useUser();
+
   return (
     <div className="flex gap-x-2 items-center">
       <button
         className="hover:scale-110 active:scale-95 transition"
-        disabled={handleDislike.isPending}
+        disabled={handleDislike.isPending || !currentUser}
       >
         {commentDisLike.data.length === 0 ? (
           <FaRegThumbsDown size={17} onClick={() => handleDislike.mutate()} />
