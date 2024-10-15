88aa24ade6cb0425c519e0a75e4c1e70b284730d
Luka Martin <ldm3334@gmail.com>
2024-10-15 21:20:49 +1100

    Revert two last commits for pull request

--- src/components/token/TokenTrades.tsx
+++ src/components/token/TokenTrades.tsx
@@ -2,11 +2,11 @@
 
 import TokenTrade from "./TokenTrade";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
-import { useEffect } from "react";
 import { Channel } from "@/models/channel";
 import { FormattedTrade } from "@/types/token/types";
 import { usePostHog } from "posthog-js/react";
-import Pusher from "pusher-js";
+import usePusher from "@/hooks/usePusher";
+import { Trade } from "@/models/trade";
 
 type TokenTradesProps = {
   trades: FormattedTrade[];
@@ -14,7 +14,7 @@ type TokenTradesProps = {
   tokenAddress: string;
 };
 
-export default function TokenTrades({ trades, tokenId, tokenAddress }: TokenTradesProps) {
+export default function TokenTrades({ trades, tokenId }: TokenTradesProps) {
   const queryClient = useQueryClient();
   const posthog = usePostHog();
 
@@ -23,45 +23,17 @@ export default function TokenTrades({ trades, tokenId, tokenAddress }: TokenTrad
     initialData: trades,
   });
 
-  useEffect(() => {
-    if (!process.env.NEXT_PUBLIC_PUSHER_CLUSTER || !process.env.NEXT_PUBLIC_PUSHER_KEY) {
-      throw new Error("Missing pusher env variables");
+  const handleTrade = ({ trade }: { trade: FormattedTrade }) => {
+    queryClient.setQueryData(["trades", tokenId], [{ ...trade, isNew: true }, ...data]);
+
+    if (trade.category === Trade.Buy) {
+      posthog.capture("token_bought", { tokenId: tokenId, trade: trade });
+    } else if (trade.category === Trade.Sell) {
+      posthog.capture("token_sold", { tokenId: tokenId, trade: trade });
     }
+  };
 
-    const pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHER_KEY, {
-      cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER,
-    });
-
-    const channels = Object.values(Channel);
-
-    const subscribedChannels = channels.map(channelName => {
-      if (channelName === Channel.Buy) {
-        return pusher.subscribe(Channel.Buy);
-      } else if (channelName === Channel.Sell) {
-        return pusher.subscribe(Channel.Sell);
-      }
-    });
-
-    subscribedChannels.forEach(channel => {
-      channel?.bind(tokenId, ({ trade }: { trade: FormattedTrade }) => {
-        queryClient.setQueryData(["trades", tokenId], [{ ...trade, isNew: true }, ...data]);
-
-        if (channel?.name === Channel.Buy) {
-          posthog.capture("token_bought", { tokenId: tokenId, trade: trade });
-        } else if (channel?.name === Channel.Sell) {
-          posthog.capture("token_sold", { tokenId: tokenId, trade: trade });
-        }
-      });
-    });
-
-    return () => {
-      subscribedChannels.forEach(channel => {
-        channel?.unbind_all();
-        channel?.unsubscribe();
-      });
-      pusher.disconnect();
-    };
-  }, [queryClient, tokenId, data, posthog, tokenAddress]);
+  usePusher(tokenId, handleTrade, [Channel.Buy, Channel.Sell]);
 
   return (
     <section className="flex flex-col">
