06f9fc797f1a62ebdbcc2ff0eb34bb3a4187f146
Luka Martin <ldm3334@gmail.com>
2024-10-17 09:26:00 +1100

    Remove unstable_cache from fetchUser query

--- src/config/user.ts
+++ src/config/user.ts
@@ -1,5 +1,3 @@
 export const defaultProfileAvatarUrl =
   "https://qojtn2d8pd7yjrch.public.blob.vercel-storage.com/Default%20Frog-KMUE9RxZ3KIHF6YGnBiR0wm4A9blOB.png";
-export const fetchUserCacheKey = "my-app-user";
-export const fetchUserCacheTag = "user";
 export const usernameDisplayLength = 8;
