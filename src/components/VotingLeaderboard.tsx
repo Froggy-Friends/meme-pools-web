3b51ac3e79f3fb2216d3ee82135664ee6b0f7729
Luka Martin <ldm3334@gmail.com>
2024-10-28 10:33:42 +1100

    Styling changes

--- src/components/VotingLeaderboard.tsx
+++ src/components/VotingLeaderboard.tsx
@@ -1,14 +1,20 @@
+"use client";
+
 import { Token } from "@prisma/client";
 import Image from "next/image";
 import bronzeMedal from "../../public/bronze-medal.svg";
 import goldMedal from "../../public/gold-medal.svg";
 import silverMedal from "../../public/silver-medal.svg";
+import { useChain } from "@/context/chain";
+import Link from "next/link";
 
 export default function VotingLeaderboard({ tokens }: { tokens: Token[] }) {
+  const { chain } = useChain();
+
   return (
     <section className="flex items-center justify-between w-full laptop:w-[75%] desktop:w-[60%] desktop:gap-6">
       {tokens.map((token, index) => (
-        <div className="flex items-center gap-1" key={token.id}>
+        <Link className="flex items-center gap-1" key={token.id} href={`/${chain.name}/token/${token.tokenAddress}`}>
           <Image
             src={index === 0 ? goldMedal : index === 1 ? silverMedal : bronzeMedal}
             alt="gold-medal"
@@ -19,9 +25,9 @@ export default function VotingLeaderboard({ tokens }: { tokens: Token[] }) {
           <span className="hidden laptop:block text-sm">{index === 0 ? "1st" : index === 1 ? "2nd" : "3rd"}</span>
 
           <span className="text-xs rounded-[4px] bg-green text-black px-2 py-1 font-proximaSoftBold">
-            $ {token.ticker}
+            ${token.ticker}
           </span>
-        </div>
+        </Link>
       ))}
     </section>
   );
