5053fdc10723823efb44d073e07df5fed85514b4
Luka Martin <ldm3334@gmail.com>
2024-11-01 22:17:00 +1100

    Update logo size in header for mobile

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -4,17 +4,19 @@ import Image from "next/image";
 import memepoolsLogo from "../../public/memepools.svg";
 import Link from "next/link";
 import { useChain } from "@/context/chain";
+import { cn } from "@nextui-org/react";
 
 type LogoProps = {
   height?: number;
   width?: number;
+  className?: string;
 };
 
-export default function Logo({ height = 70, width = 70 }: LogoProps) {
+export default function Logo({ height = 70, width = 70, className }: LogoProps) {
   const { chain } = useChain();
 
   return (
-    <Link href={`/${chain.name}`} className="hover:scale-[1.02] transition">
+    <Link href={`/${chain.name}`} className={cn("hover:scale-[1.02] transition", className)}>
       <Image src={memepoolsLogo} alt="Meme Pools" height={height} width={width} />
     </Link>
   );
