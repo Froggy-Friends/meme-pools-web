a9c86322a0d2818ae0c3efe9f2a3da8bc0ea12c9
Luka Martin <ldm3334@gmail.com>
2024-11-05 21:16:46 +1100

    Add optional chaining

--- src/lib/getEthPrice.ts
+++ src/lib/getEthPrice.ts
@@ -1,5 +1,5 @@
 export default async function getEthPrice() {
   const res = await fetch("https://api.coinbase.com/v2/prices/ETH-USD/spot");
   const data = await res.json();
-  return Number(data.data.amount);
+  return Number(data.data.amount || 0);
 }
