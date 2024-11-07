2b0940c72a2eabacb842599de0f00102c5777d57
Luka Martin <ldm3334@gmail.com>
2024-11-07 22:54:57 +1100

    Add bubblemaps component

---
+++ src/components/token/BubbleMaps.tsx
@@ -0,0 +1,7 @@
+export default function BubbleMaps() {
+  return (
+    <section className="bg-dark rounded-lg flex justify-center items-center w-full h-[300px] tablet:h-[580px]">
+      <p>Bubblemaps Coming Soon.</p>
+    </section>
+  );
+}
