445c850a235d827d5ae5ee91ac613d2838281786
Luka Martin <145173880+LukaMartin@users.noreply.github.com>
2024-11-26 15:35:43 +1100

    Luka/purchased coins (#152)

--- src/components/profile/ProfileMenuToggle.tsx
+++ src/components/profile/ProfileMenuToggle.tsx
@@ -24,6 +24,9 @@ export default function ProfileMenuToggle({ profileUser, cachedUser, currentView
     <div className="overflow-x-auto">
       <section className="flex gap-x-4 items-center my-6 min-w-max">
         {cachedUser?.id === profileUser.id && (
+          <ProfileMenuButton view="Holdings" profileUser={profileUser} currentView={currentView} />
+        )}
+        {cachedUser?.id === profileUser.id && (
           <ProfileMenuButton view="Settings" profileUser={profileUser} currentView={currentView} />
         )}
         <ProfileMenuButton view="Created" profileUser={profileUser} currentView={currentView} />
---
+++ src/components/profile/UserHoldings.tsx
@@ -0,0 +1,36 @@
+import { fetchUserHoldings } from "@/queries/profile/queries";
+import { User } from "@prisma/client";
+import Link from "next/link";
+import PurchasedCoinCard from "./PurchasedCoinCard";
+import { useMemo } from "react";
+
+type UserHoldingsProps = {
+  profileUser: User;
+  cachedUser: User | null;
+};
+
+export default async function UserHoldings({ profileUser, cachedUser }: UserHoldingsProps) {
+  const holdings = await fetchUserHoldings(profileUser.id);
+  let enabled = profileUser.id === cachedUser?.id;
+  const sortedHoldings = useMemo(() => holdings.sort((a, b) => (b.marketCap ?? 0) - (a.marketCap ?? 0)), [holdings]);
+
+  return (
+    <section className="mt-10 mb-12 flex flex-col gap-y-2 bg-dark-gray rounded-xl p-6">
+      <p className="text-2xl mb-4">Holdings</p>
+      {!holdings.length && cachedUser && cachedUser.id === profileUser.id && (
+        <p className="ml-1 -mt-8">
+          <Link href="/" className="text-primary hover:text-light-primary transition">
+            Buy
+          </Link>{" "}
+          your first coin
+        </p>
+      )}
+      {!holdings.length && cachedUser && cachedUser.id !== profileUser.id && (
+        <p className="ml-1 -mt-8">No coins purchased</p>
+      )}
+      {sortedHoldings.map(token => {
+        return <PurchasedCoinCard key={token.id} token={token} enabled={enabled} />;
+      })}
+    </section>
+  );
+}
