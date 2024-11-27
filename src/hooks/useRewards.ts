49652d0bb82b7cc2a1d1b46df63164446540360b
Luka Martin <ldm3334@gmail.com>
2024-11-27 13:29:22 +1100

    Add hook for rewards

---
+++ src/hooks/useRewards.ts
@@ -0,0 +1,33 @@
+import { mempoolsClaimsAbi } from "@/abi/mempoolsClaims";
+import { claimContractAddress } from "@/config/env";
+import { Address } from "viem";
+import { useEthersProvider } from "@/config/eth/wagmi-ethers";
+import { ethers } from "ethers";
+import { getFrogsByWallet } from "@/lib/getFrogsByWallet";
+import useUser from "./useUser";
+import { formatUnits } from "viem";
+import { useState } from "react";
+
+export default function useRewards() {
+  const [pending, setPending] = useState(false);
+  const { currentUser } = useUser();
+  const provider = useEthersProvider();
+  const contract = new ethers.Contract(
+    claimContractAddress,
+    mempoolsClaimsAbi,
+    provider
+  );
+
+  const fetchRewards = async (tokenAddress: Address) => {
+    setPending(true);
+    if (!currentUser || !tokenAddress) return 0;
+
+    const frogIds = await getFrogsByWallet(currentUser?.ethAddress as Address);
+    const rewards = await contract.rewards(tokenAddress, frogIds);
+
+    setPending(false);
+    return formatUnits(rewards, 18);
+  };
+
+  return { fetchRewards, pending };
+}
