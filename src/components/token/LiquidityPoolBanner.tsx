99e627073b3fa5f51908bd801c698f4341a9ede0
Luka Martin <145173880+LukaMartin@users.noreply.github.com>
2024-11-28 12:18:27 +1100

    Add dynamic message to LP banner (#155)

--- src/components/token/LiquidityPoolBanner.tsx
+++ src/components/token/LiquidityPoolBanner.tsx
@@ -1,7 +1,10 @@
 "use client";
 
+import { useChain } from "@/context/chain";
 import useTokenInfo from "@/hooks/useTokenInfo";
 import { Token } from "@prisma/client";
+import Link from "next/link";
+import { FaExternalLinkAlt } from "react-icons/fa";
 
 type LiquidityPoolBannerProps = {
   token: Token;
@@ -9,12 +12,32 @@ type LiquidityPoolBannerProps = {
 
 export default function LiquidityPoolBanner({ token }: LiquidityPoolBannerProps) {
   const { tokenInfo } = useTokenInfo(token);
+  const { chain } = useChain();
 
   return (
     <>
       {tokenInfo?.readyForLp && (
         <section className="w-full h-12 tablet:h-20 mb-8 tablet:mb-12 flex items-center justify-center bg-primary rounded-lg tablet:text-3xl">
-          <p className="text-black">All coins sold, liquidity pool coming soon!</p>
+          {tokenInfo?.liquidityPoolSeeded ? (
+            <p className="text-black flex items-center gap-x-2">
+              All coins sold, trade now on {chain.name === "solana" ? "Raydium" : "Uniswap"}
+              <Link
+                href={
+                  chain.name === "solana"
+                    ? `https://raydium.io/swap/?inputMint=So11111111111111111111111111111111111111112&outputMint=${token.tokenAddress}`
+                    : `https://app.uniswap.org/explore/tokens/${chain.name === "eth" ? "ethereum" : chain.name}/${
+                        token.tokenAddress
+                      }`
+                }
+                target="_blank"
+                className="text-black hover:text-dark hover:scale-[1.04] transition"
+              >
+                <FaExternalLinkAlt size={20} className="mt-1" />
+              </Link>
+            </p>
+          ) : (
+            <p className="text-black">All coins sold, liquidity pool coming soon!</p>
+          )}
         </section>
       )}
     </>
