930261ac02a71977a1857071df69d96f3befabf0
Luka Martin <ldm3334@gmail.com>
2024-12-12 15:06:49 +1100

    Minor changes

--- src/components/token/TokenInfo.tsx
+++ src/components/token/TokenInfo.tsx
@@ -15,12 +15,20 @@ type TokenInfoParams = {
 export default function TokenInfo({ token, creator }: TokenInfoParams) {
   return (
     <section className="flex flex-col w-full my-2 gap-y-4 pb-10">
-      <div className="flex items-center gap-4">
-        <Image src={token.image} alt="token-image" height={50} width={50} className="h-[50px] w-[50px] rounded-3xl" />
-        <p className="hidden tablet:block text-6xl font-proximaNovaBold">${token.ticker}</p>
-        <p className="tablet:hidden text-5xl font-proximaNovaBold">${formatTicker(token.ticker)}</p>
-      </div>
+      <div className="flex justify-between items-center">
+        <div className="flex items-center gap-4">
+          <Image src={token.image} alt="token-image" height={50} width={50} className="h-[50px] w-[50px] rounded-3xl" />
+          <p className="hidden tablet:block text-6xl font-proximaNovaBold">${token.ticker}</p>
+          <p className="tablet:hidden text-5xl font-proximaNovaBold">${formatTicker(token.ticker)}</p>
+        </div>
 
+        <Link
+          href={`/memepool/${token.tokenAddress}`}
+          className="bg-primary text-black flex items-center justify-center font-proximaNovaBold px-4 h-[30px] rounded-xl hover:bg-light-primary disabled:bg-gray active:scale-[0.98] transition"
+        >
+          Go to Meme Pool
+        </Link>
+      </div>
       <div className="flex items-center gap-x-2 pl-3">
         {creator && (
           <Link href={`/profile/${creator.name}`} className="text-light-primary hover:text-cream transition">
