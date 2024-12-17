98634c83e331b9d7c1d3a53c0f273eb5d963b3bc
Luka Martin <ldm3334@gmail.com>
2024-12-17 23:07:35 +1100

    Update env.ts config

--- src/config/env.ts
+++ src/config/env.ts
@@ -17,14 +17,14 @@ export const memepoolsApi = isProd
 // evm
 export const contractAddress: Address = isProd
   ? "0x4bA69e95F5F18902Cad88a1e35E037F1DC7E7b77"
-  : "0xA6d2E7e8984D57a95F0DB6Ae06087cA43E20B3E8";
+  : "0xEFd94EDFCB5902dc3Ce1F379DDB006Df0326Dc34";
 export const froggyFriendsAddress: Address = isProd
   ? "0x7ad05c1b87e93BE306A9Eadf80eA60d7648F1B6F"
   : "0xc8939011efd81fB0ca8382ed15EAb160c3a69313";
 export const claimContractAddress: Address = isProd
   ? "0x5D606A1eB8Cd7c50092866725B9982355b4a5F4C"
-  : "0x5D606A1eB8Cd7c50092866725B9982355b4a5F4C";
-export const contractAddressV1 = "0x4bA69e95F5F18902Cad88a1e35E037F1DC7E7b77";
+  : "0x6570Ff390976C206b05EcD5873a0aAaF7a1F3D28";
+export const contractAddressV1 = "0xA6d2E7e8984D57a95F0DB6Ae06087cA43E20B3E8";
 export const ethChainId = isProd ? 1 : 11155111;
 export const baseChainId = isProd ? 8453 : 84532;
 export const ethChain = isProd ? mainnet : sepolia;
