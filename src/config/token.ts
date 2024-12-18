bbbf9c0196389e46b73992d4326a3a020df67584
Luka Martin <ldm3334@gmail.com>
2024-12-18 12:20:40 +1100

    Add constant for token launch buffer

--- src/config/token.ts
+++ src/config/token.ts
@@ -1,3 +1,4 @@
 export const ethMarketcapGoalV1 = 15.9;
 export const ethMarketcapGoalV2 = 13.1;
 export const tokensPerPage = 100;
+export const tokenLaunchBuffer = 100;
--- src/lib/getBondingCurvePercentage.ts
+++ src/lib/getBondingCurvePercentage.ts
@@ -1,8 +1,10 @@
+import { tokenLaunchBuffer } from "@/config/token";
 import { maxTradeableSupply } from "./constants";
 
 export const getBondingCurvePercentage = (tokensSold: number | undefined) => {
   if (!tokensSold) return 0;
-  const percentage = (tokensSold / (maxTradeableSupply - 1)) * 100;
+  const percentage =
+    (tokensSold / (maxTradeableSupply - tokenLaunchBuffer)) * 100;
 
   return percentage === 100 ? 100 : Math.floor(Number(percentage));
 };
