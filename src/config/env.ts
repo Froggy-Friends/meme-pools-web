3ee4af13d43374da2f9a7ffedf9038eae89bb6c6
Luka Martin <ldm3334@gmail.com>
2024-12-20 00:42:49 +1100

    Update env config

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,5 +1,5 @@
 import { Address } from "viem";
-import { base, baseSepolia, mainnet, sepolia } from "viem/chains";
+import { base, mainnet, sepolia } from "viem/chains";
 
 export const env = process.env.NEXT_PUBLIC_ENV;
 export const isProd = env === "prod";
@@ -17,19 +17,24 @@ export const memepoolsApi = isProd
 // evm
 export const contractAddress: Address = isProd
   ? "0x4bA69e95F5F18902Cad88a1e35E037F1DC7E7b77"
-  : "0xEFd94EDFCB5902dc3Ce1F379DDB006Df0326Dc34";
+  : "0x1e6AaaC4201CdDd10b90735898f71Fd3541aF66F";
+export const baseContractAddress: Address =
+  "0x5D606A1eB8Cd7c50092866725B9982355b4a5F4C";
 export const froggyFriendsAddress: Address = isProd
   ? "0x7ad05c1b87e93BE306A9Eadf80eA60d7648F1B6F"
   : "0xc8939011efd81fB0ca8382ed15EAb160c3a69313";
 export const claimContractAddress: Address = isProd
   ? "0x5D606A1eB8Cd7c50092866725B9982355b4a5F4C"
   : "0x6570Ff390976C206b05EcD5873a0aAaF7a1F3D28";
-export const contractAddressV1 = "0xA6d2E7e8984D57a95F0DB6Ae06087cA43E20B3E8";
+export const baseClaimContractAddress: Address =
+  "0xb6224A473Edd4E401B4f86650ec58eDb124596eF";
+export const contractAddressV1 = "0xEFd94EDFCB5902dc3Ce1F379DDB006Df0326Dc34";
 export const ethChainId = isProd ? 1 : 11155111;
-export const baseChainId = isProd ? 8453 : 84532;
+export const baseChainId = 8453;
 export const ethChain = isProd ? mainnet : sepolia;
-export const baseChain = isProd ? base : baseSepolia;
+export const baseChain = base;
 export const moralisEthChain = isProd ? "eth" : "sepolia";
+export const moralisBaseChain = "base";
 
 // solana
 export const solanaChainId = isProd ? 0 : 0;
@@ -41,7 +46,5 @@ export const walletConnectProjectId = "28fb63c6591b078e4eca73e608508aec";
 export const etherscanUrl = isProd
   ? "https://etherscan.io"
   : "https://sepolia.etherscan.io";
-export const baseExplorerUrl = isProd
-  ? "https://basescan.org"
-  : "https://sepolia.basescan.org";
+export const baseExplorerUrl = "https://basescan.org";
 export const solanaExplorerUrl = "https://solscan.io";
