cca69714550c16da0980c23364807facbfb79a6e
Luka Martin <ldm3334@gmail.com>
2024-12-20 00:41:26 +1100

    Filter coins in profil tabs by chain

--- src/components/profile/Claim.tsx
+++ src/components/profile/Claim.tsx
@@ -4,18 +4,20 @@ import { User } from "@prisma/client";
 import { fetchClaimableTokens } from "@/queries/profile/queries";
 import ClaimableTokenCard from "./ClaimableTokenCard";
 import { getFrogsByWallet } from "@/lib/getFrogsByWallet";
+import { Chain } from "@/models/chain";
 
 type ClaimTokensProps = {
   profileUser: User;
   cachedUser: User | null;
   delegatedWallets: Address[];
+  chain: Chain;
 };
 
-export default async function ClaimTokens({ profileUser, cachedUser, delegatedWallets }: ClaimTokensProps) {
+export default async function ClaimTokens({ profileUser, cachedUser, delegatedWallets, chain }: ClaimTokensProps) {
   const walletAddresses = delegatedWallets ? [...delegatedWallets, profileUser.ethAddress] : [profileUser.ethAddress];
   let enabled = profileUser.id === cachedUser?.id;
   const frogIds = await getFrogsByWallet(profileUser.ethAddress as Address);
-  const claimableTokens = await fetchClaimableTokens(frogIds);
+  const claimableTokens = await fetchClaimableTokens(frogIds, chain);
 
   return (
     <section className="mt-10 mb-12 flex flex-col gap-y-2 bg-dark-gray rounded-xl p-4 tablet:p-6 w-full tablet:w-[75%] laptop:w-[66%]">
--- src/lib/chains.ts
+++ src/lib/chains.ts
@@ -2,11 +2,11 @@ import { chainConfigs } from "@/config/chains";
 import { Chain, ChainConfig } from "@/models/chain";
 
 export function getChainConfig(path: string): ChainConfig {
-  if (path === `/${Chain.Eth}`) {
+  if (path === `/${Chain.Eth}` || path === Chain.Eth) {
     return chainConfigs.eth;
-  } else if (path === `/${Chain.Base}`) {
+  } else if (path === `/${Chain.Base}` || path === Chain.Base) {
     return chainConfigs.base;
-  } else if (path === `/${Chain.Solana}`) {
+  } else if (path === `/${Chain.Solana}` || path === Chain.Solana) {
     return chainConfigs.solana;
   } else {
     return chainConfigs.eth;
