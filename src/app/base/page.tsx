82d2ec06d1d44e397417a98dec4e267b48c114c7
Luka Martin <ldm3334@gmail.com>
2024-12-20 16:17:14 +1100

    Remove hydration boundary

--- src/app/base/page.tsx
+++ src/app/base/page.tsx
@@ -4,23 +4,19 @@ import LeaderBoardAndFeedContainer from "@/components/LeaderBoardAndFeedContaine
 import Spotlight from "@/components/Spotlight";
 import TokenPageContent from "@/components/TokenPageContent";
 import { Chain } from "@/models/chain";
-import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query";
 
 export default async function BaseHomePage() {
-  const queryClient = new QueryClient();
-
   return (
     <main className="flex flex-col min-h-[100vh] max-w-[410px] tablet:max-w-[750px] laptop:max-w-[924px] desktop:max-w-[1200px] mx-auto px-2 tablet:px-4">
       <Header chain={Chain.Base} />
-      <HydrationBoundary state={dehydrate(queryClient)}>
-        <div className="flex flex-col gap-6 w-full">
-          <LeaderBoardAndFeedContainer />
 
-          <Spotlight />
-          
-          <TokenPageContent />
-        </div>
-      </HydrationBoundary>
+      <div className="flex flex-col gap-6 w-full">
+        <LeaderBoardAndFeedContainer />
+
+        <Spotlight />
+
+        <TokenPageContent />
+      </div>
 
       <Footer />
     </main>
