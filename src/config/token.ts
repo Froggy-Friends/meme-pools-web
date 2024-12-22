a108ab8ff5b716554419f0faf68406a4984134ca
Luka Martin <ldm3334@gmail.com>
2024-12-22 15:09:27 +1100

    Update constants

--- src/config/token.ts
+++ src/config/token.ts
@@ -1,11 +1,12 @@
 export const ethMarketcapGoalV1 = 15.9;
-export const ethMarketcapGoalV2 = 13.1;
+export const ethMarketcapGoalV2 = 14.4;
 export const tokensPerPage = 100;
 export const tokenLaunchBuffer = 100;
 export const MAX_MARKET_CAP = 42_000;
 export const maxReservedSupply = 10_000_000;
 export const maxTotalSupply = 1_000_000_000;
 export const maxTradeableSupply = 800_000_000;
+export const maxTradeableSupplyBase = 750_000_000;
 export const tierOneEthReward = 0.2;
 export const tierTwoEthReward = 0.4;
 export const tierThreeEthReward = 0.6;
