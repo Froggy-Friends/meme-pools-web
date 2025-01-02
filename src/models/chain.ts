e1e2025a773724bbaabd11ff8da4840b5d11b4a3
Luka Martin <ldm3334@gmail.com>
2025-01-02 16:28:40 +1100

    Add apechain relevant configs

--- src/models/chain.ts
+++ src/models/chain.ts
@@ -4,6 +4,7 @@ export enum Chain {
   Solana = "solana",
   Base = "base",
   Eth = "eth",
+  ApeChain = "apechain",
 }
 
 export type ChainConfig = {
@@ -17,4 +18,5 @@ export type ChainConfigs = {
   eth: ChainConfig;
   base: ChainConfig;
   solana: ChainConfig;
+  apechain: ChainConfig;
 };
